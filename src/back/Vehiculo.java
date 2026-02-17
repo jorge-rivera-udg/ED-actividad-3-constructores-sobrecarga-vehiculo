02591d3f9663e8247eb222690927412c3ec28728
Jorge Rivera <jorge.rivera@ssde.com.mx>
2026-02-17 04:49:48 -0600

    ED-A3. Agregar comentario con nombres de participanrtes de equipo a clase vehiculo

--- src/back/Vehiculo.java
+++ src/back/Vehiculo.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Ambriz Cordero Diego Said
+ * Medeles Bryan Jonathan
+ * Rivera Andrade Jorge
  */
 package back;
 
